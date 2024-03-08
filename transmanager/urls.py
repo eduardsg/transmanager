ce90d99180594360b507b68856ac1982c9da7655
Eduard Solivellas <esolivellas@apsl.net>
2024-03-08 13:45:04 +0100

    fix url.w002

--- transmanager/urls.py
+++ transmanager/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from .views import TaskListView, TaskDetailView, TaskBulksView, UploadTranslationsView, MessageView, DownloadFileView
 
 urlpatterns = [
-    path('/task/', TaskListView.as_view(), name='transmanager-task-list'),
-    path('/task/<int:pk>/', TaskDetailView.as_view(), name='transmanager-task-detail'),
-    path('/api/task/', TaskBulksView.as_view(), name='transmanager-task-bulks'),
-    path('/upload-translations/', UploadTranslationsView.as_view(), name='transmanager-upload-translations'),
-    path('/message/', MessageView.as_view(), name='transmanager-message'),
-    path('/download-file/<uuid:uuid>/', DownloadFileView.as_view(), name='transmanager-download-file'),
+    path('task/', TaskListView.as_view(), name='transmanager-task-list'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='transmanager-task-detail'),
+    path('api/task/', TaskBulksView.as_view(), name='transmanager-task-bulks'),
+    path('upload-translations/', UploadTranslationsView.as_view(), name='transmanager-upload-translations'),
+    path('message/', MessageView.as_view(), name='transmanager-message'),
+    path('download-file/<uuid:uuid>/', DownloadFileView.as_view(), name='transmanager-download-file'),
 ]
